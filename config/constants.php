69adfc144d96f22747c12e04eff1f7101a80327b
kunjgit <103763618+kunjgit@users.noreply.github.com>
2023-05-05 09:46:29 +0530

    made the database changes

--- config/constants.php
+++ config/constants.php
@@ -2,11 +2,11 @@
     
     session_start();
     //Create Constants to Store Non Repeating Values
-    define('SITEURL', 'http://localhost/onlinefood-order/');
+    define('SITEURL', 'http://localhost/zayka/');
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'zayka');
+    define('DB_NAME', 'zayka1');
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
